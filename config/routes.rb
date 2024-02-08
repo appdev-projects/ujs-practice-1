46eabf82de767253167d6ae84a9f909aee65c03a
Ben Purinton <ben.purinton@gmail.com>
2024-02-08 09:56:28 -0900

    add task partial

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   
-  resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tasks do
+    patch "move", on: :member
+  end
 end
