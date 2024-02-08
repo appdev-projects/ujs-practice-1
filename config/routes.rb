0607e397b46adc9cc0fd1045c98e8043193cf804
Ben Purinton <ben.purinton@gmail.com>
2024-02-08 09:27:20 -0900

    force user sign in, set root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :tasks
+  root "tasks#index"
+  
   devise_for :users
+  
+  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
