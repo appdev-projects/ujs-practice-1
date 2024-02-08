6908918760ce41642f044049e90dd30c991edb89
Ben Purinton <ben.purinton@gmail.com>
2024-02-08 09:20:01 -0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_08_180945) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_08_181245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.string "satus", default: "not_yet_started", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_08_180945) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
