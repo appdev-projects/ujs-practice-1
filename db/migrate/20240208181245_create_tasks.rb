8d5fc3c79802e06eb9508ed7fffc9b788c90855b
Ben Purinton <ben.purinton@gmail.com>
2024-02-08 09:19:04 -0900

    db null constraint for task

--- db/migrate/20240208181245_create_tasks.rb
+++ db/migrate/20240208181245_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.text :content
+      t.text :content, null: false
       t.references :user, null: false, foreign_key: true
-      t.string :satus, default: "not_yet_started"
+      t.string :satus, default: "not_yet_started", null: false
 
       t.timestamps
     end
